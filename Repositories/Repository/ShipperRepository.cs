c136a8647bd2cfba484b51f8a13029e2cc16daa2
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:40:21 +0300

    Add ShipperRepository

---
+++ Repositories/Repository/ShipperRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+using NorthwindBasedWebAPI.Data;
+using NorthwindBasedWebAPI.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class ShipperRepository : EntityBaseRepository<Shipper>, IShipperRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public ShipperRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Order>> GetOrdersByShipper(int id)
+        {
+            var orders = await _context.Orders
+                .Where(i => i.ShipVia == id)
+                .ToListAsync();
+
+            return orders;
+        }
+    }
+}
