27d88a5a8c6eed194144dfbb006a58309bb8d40d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:37:24 +0300

    Add IOrderRepository

---
+++ Repositories/IRepository/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface IOrderRepository : IEntityBaseRepository<Order>
+    {
+    }
+}
