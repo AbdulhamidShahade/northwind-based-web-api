ace40ec3b1e038d9e8619dd93818e176d8de2c2b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:37:02 +0300

    Add ICustomerRepository

---
+++ Repositories/IRepository/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface ICustomerRepository : IEntityBaseRepository<Customer>
+    {
+        Task<ICollection<Order>> GetOrdersByCustomerAsync(int id);
+        Task<ICollection<CustomerDemographic>> GetCustomerDemographicsByCustomerAsync(int id);
+    }
+}
