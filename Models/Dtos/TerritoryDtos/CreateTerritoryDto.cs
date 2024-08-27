8190e15e84b97132e3c16e5b6249475198ef81cc
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:43:11 +0300

    Add territory dtos

---
+++ Models/Dtos/TerritoryDtos/CreateTerritoryDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.TerritoryDTOs
+{
+    public class CreateTerritoryDto
+    {
+
+
+        [Required(ErrorMessage = "Territory Description is required!")]
+        [Display(Name = "Territory Description")]
+        public string TerritoryDescription { get; set; }
+
+
+        [Display(Name = "PictureUrl")]
+        public string? PictureUrl { get; set; }
+
+
+
+    
+
+    }
+}
---
+++ Models/Dtos/TerritoryDtos/ReadTerritoryDto.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.TerritoryDTOs
+{
+    public class ReadTerritoryDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Territory Description")]
+        public string TerritoryDescription { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+        [Display(Name = "Rojin")]
+        public int RegionId { get; set; }
+
+
+    }
+}
