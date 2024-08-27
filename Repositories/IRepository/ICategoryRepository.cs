b1eed07694d63dc4c648da512e96df875423170c
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:36:32 +0300

    Add ICategoryRepository

---
+++ Repositories/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface ICategoryRepository : IEntityBaseRepository<Category>
+    {
+        Task<ICollection<Product>> GetProductsByCategory(int id);
+    }
+}
