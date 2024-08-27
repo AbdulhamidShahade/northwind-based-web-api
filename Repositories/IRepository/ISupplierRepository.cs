06eda29c582abef5b2c91322c4e649e22c840150
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:38:15 +0300

    Add ISupplierRepository

---
+++ Repositories/IRepository/ISupplierRepository.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface ISupplierRepository : IEntityBaseRepository<Supplier>
+    {
+        Task<ICollection<Product>> GetProductsBySupplier(int id);
+    }
+}
