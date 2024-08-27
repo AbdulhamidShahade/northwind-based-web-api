150bb909c3b7904130de54e6982c94b4468563ed
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:40:12 +0300

    Add RegionRepository

---
+++ Repositories/Repository/RegionRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+using NorthwindBasedWebAPI.Data;
+using NorthwindBasedWebAPI.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class RegionRepository : EntityBaseRepository<Region>, IRegionRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public RegionRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Territory>> GetTerritoriesByRegionAsync(int id)
+        {
+            var territories = await _context.Territories
+                .Where(i => i.RegionId == id)
+                .ToListAsync();
+
+            return territories;
+        }
+    }
+}
