832a77c9bdd70be02c50acf736ff21ce3642bedf
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:50:59 +0300

    Add customer demographic domain model

---
+++ Models/CustomerDemographic.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class CustomerDemographic
+    {
+        [Display(Name = "Customer Description")]
+        public string? CustomerDescription { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+    }
+}
