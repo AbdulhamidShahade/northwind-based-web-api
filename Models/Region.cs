5bd7b18d4ecd4fa86572ba3c7b983c59fb150f58
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:45:09 +0300

    Add region domain model

---
+++ Models/Region.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class Region
+    {
+        [Required(ErrorMessage = "Region description is required!")]
+        [Display(Name = "Region Description")]
+        public string RegionDescription { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+    }
+}
