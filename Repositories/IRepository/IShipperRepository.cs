ba9da0d9745bdf0ce5047c7faa5022d9328e0c71
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:38:05 +0300

    Add IShipperRepository

---
+++ Repositories/IRepository/IShipperRepository.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface IShipperRepository : IEntityBaseRepository<Shipper>
+    {
+        Task<ICollection<Order>> GetOrdersByShipper(int id);
+    }
+}
