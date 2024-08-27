861cbbcf1895cb4b920e6ea6ab74416505519d74
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:28:44 +0300

    Add category domain model

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class Category
+    {
+        [Display(Name = "Category Name")]
+        [Required(ErrorMessage = "Category name is required field!")]
+        public string CategoryName { get; set; }
+
+
+
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+    }
+}
