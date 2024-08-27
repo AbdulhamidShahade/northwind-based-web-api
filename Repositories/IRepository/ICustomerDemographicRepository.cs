e590a11f49d346ce06413868c3a39859e6387420
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:36:51 +0300

    Add ICustomerDemographicRepository

---
+++ Repositories/IRepository/ICustomerDemographicRepository.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface ICustomerDemographicRepository : IEntityBaseRepository<CustomerDemographic>
+    {
+        Task<ICollection<Customer>> GetCustomersByCustomerDemographicAsync(int id);
+    }
+}
