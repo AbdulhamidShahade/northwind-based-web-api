90adbd0dccf8980f208699506299c0c182d51529
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:47:31 +0300

    Add territory domain model

---
+++ Models/Territory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class Territory
+    {
+        [Required]
+        [Display(Name = "Territory Description")]
+        public string TerritoryDescription { get; set; }
+
+
+
+        [Display(Name = "PictureUrl")]
+        public string? PictureUrl { get; set; }
+    }
+}
