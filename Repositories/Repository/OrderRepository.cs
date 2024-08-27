90ced9938cc433664b542f75219ed052fdc98d11
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:39:51 +0300

    Add OrderRepository

---
+++ Repositories/Repository/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+using NorthwindBasedWebAPI.Data;
+using NorthwindBasedWebAPI.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class OrderRepository : EntityBaseRepository<Order>, IOrderRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
