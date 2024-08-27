9571606924dbf2bbde4ea0f2ac7354ee2c6ee137
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:54:10 +0300

    Add EmployeeTerritory domain model

---
+++ Models/EmployeeTerritory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class EmployeeTerritory
+    {
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+
+
+        [ForeignKey("Territory")]
+        public int TerritoryId { get; set; }
+        public Territory Territory { get; set; }
+    }
+}
