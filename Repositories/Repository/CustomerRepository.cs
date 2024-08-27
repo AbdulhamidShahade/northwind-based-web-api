c095592dcbecc76d38e69afd0213600d6ca83900
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:39:33 +0300

    Add CustomerRepository

---
+++ Repositories/Repository/CustomerRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+using NorthwindBasedWebAPI.Data;
+using NorthwindBasedWebAPI.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class CustomerRepository : EntityBaseRepository<Customer>, ICustomerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<CustomerDemographic>> GetCustomerDemographicsByCustomerAsync(int id)
+        {
+            var customerDemographic = await _context.CustomersCustomerDemographics
+                .Where(i => i.CustomerId == id)
+                .Select(c => c.CustomerType)
+                .OrderBy(i => i.Id)
+                .ToListAsync();
+
+            return customerDemographic;
+        }
+
+        public async Task<ICollection<Order>> GetOrdersByCustomerAsync(int id)
+        {
+            var orders = await _context.Orders
+                .Where(i => i.CustomerId == id)
+                .OrderBy(i => i.Id)
+                .ToListAsync();
+
+            return orders;
+        }
+    }
+}
