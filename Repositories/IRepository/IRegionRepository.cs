fa37bb41c70a306280dadeb91b866b94492ece83
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:37:55 +0300

    Add IRegionRepository

---
+++ Repositories/IRepository/IRegionRepository.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface IRegionRepository : IEntityBaseRepository<Region>
+    {
+        Task<ICollection<Territory>> GetTerritoriesByRegionAsync(int id);
+    }
+}
