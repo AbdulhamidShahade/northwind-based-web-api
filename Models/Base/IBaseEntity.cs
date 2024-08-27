252ecf7eeed748c5142799457b7d5668bc730697
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 16:43:54 +0300

    Add IBaseEntity

---
+++ Models/Base/IBaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace NorthwindBasedWebAPI.Models.Base
+{
+    public interface IBaseEntity
+    {
+        int Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+    }
+}
