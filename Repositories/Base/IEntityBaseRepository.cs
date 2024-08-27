d90f6b77462741fac1f8d9435a65f42e5ec29e64
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 16:44:30 +0300

    Add IEntityBaseRepository

---
+++ Repositories/Base/IEntityBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using NorthwindBasedWebAPI.Models.Base;
+using System.Linq.Expressions;
+
+namespace NorthwindBasedWebAPI.Repositories.Base
+{
+    public interface IEntityBaseRepository<T> where T : IBaseEntity
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null,
+            bool tracked = true, string? includeProperties = null);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate = null,
+            bool tracked = true, string? includeProperties = null);
+
+        Task<bool> CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> IsExistAsync(Expression<Func<T, bool>> predicate = null,
+            bool tracked = true, string? includeProperties = null);
+
+
+        Task<bool> DeleteAsync(T entity);
+    }
+}
