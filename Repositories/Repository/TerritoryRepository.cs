d2adbb3becf086b41af490cc13d18914e69781b4
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:40:40 +0300

    Add TerritoryRepository

---
+++ Repositories/Repository/TerritoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebAPI.Data;
+using NorthwindBasedWebAPI.Repositories.IRepository;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class TerritoryRepository : EntityBaseRepository<Territory>, ITerritoryRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public TerritoryRepository(ApplicationDbContext context) : base (context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Employee>> GetEmployeesByTerritoryAsync(int id)
+        {
+            var employees = await _context.EmployeesTerritories
+                .Where(i => i.TerritoryId == id)
+                .Select(e => e.Employee)
+                .ToListAsync();
+
+            return employees;
+        }
+
+        public async Task<Region> GetRegionByTerritoryAsync(int id)
+        {
+            var region = await _context.Territories
+                .Where(i => i.Id == id)
+                .Select(r => r.Region)
+                .FirstOrDefaultAsync();
+
+            return region;
+        }
+    }
+}
