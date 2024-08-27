23a3d677b38569a0d89d209aa8547e0e582d8515
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:43:36 +0300

    Add order domain model

---
+++ Models/Order.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class Order
+    {
+        [Required(ErrorMessage = "Order date is required field!")]
+        [Display(Name = "Order Date")]
+        public DateTime OrderDate { get; set; }
+
+
+
+        [Required(ErrorMessage = "Required date is required field!")]
+        [Display(Name = "Required Date")]
+        public DateTime RequiredDate { get; set; }
+
+
+
+        [Display(Name = "Shipped Date")]
+        public DateTime? ShippedDate { get; set; }
+
+
+
+        [Required(ErrorMessage = "Freight is required field!")]
+        [Display(Name = "Freight")]
+        public decimal Freight { get; set; }
+
+
+
+        [Required(ErrorMessage = "Ship name is required field!")]
+        [Display(Name = "Ship Name")]
+        public string ShipName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Shipped address is required field!")]
+        [Display(Name = "Shipped Address")]
+        public string ShipAddress { get; set; }
+
+
+
+        [Required(ErrorMessage = "Ship city is required field!")]
+        [Display(Name = "Ship City")]
+        public string ShipCity { get; set; }
+
+
+
+        [Display(Name = "Ship Region")]
+        public string? ShipRegion { get; set; }
+
+
+
+        [Display(Name = "Ship Postal Code")]
+        public string? ShipPostalCode { get; set; }
+
+
+
+        [Required(ErrorMessage = "Ship country is required field!")]
+        [Display(Name = "Ship Country")]
+        public string ShipCountry { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+    }
+}
