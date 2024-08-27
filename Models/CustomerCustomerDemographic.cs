0929c3808ed4562e9af281ccdbef00138cb5af2b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 15:52:53 +0300

    Add CustomerCustomerDemographic domain model

---
+++ Models/CustomerCustomerDemographic.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class CustomerCustomerDemographic
+    {
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+
+
+        [ForeignKey("CustomerType")]
+        public int CustomerTypeId { get; set; }
+        public CustomerDemographic CustomerType { get; set; }
+    }
+}
