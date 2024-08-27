88074df156c2a9401d82c72a245d7a6a537523c7
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:37:13 +0300

    Add IEmployeeRepository

---
+++ Repositories/IRepository/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface IEmployeeRepository : IEntityBaseRepository<Employee>
+    {
+        Task<ICollection<Territory>> GetTerritoriesByEmployeeAsync(int id);
+        Task<ICollection<Order>> GetOrdersByEmployeeAsync(int id);
+    }
+}
