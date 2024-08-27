0305080004472761a989dfc1e029d77004359c76
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-27 16:43:43 +0300

    Add BaseEntity

---
+++ Models/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models.Base
+{
+    public class BaseEntity : IBaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
