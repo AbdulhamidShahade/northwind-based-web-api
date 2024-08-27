ce6f066730b1f4b8fcfd7ae51238646a2d92badc
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:37:40 +0300

    Add IProductRepository

---
+++ Repositories/IRepository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface IProductRepository : IEntityBaseRepository<Product>
+    {
+        Task<Category> GetCategoryByProductAsync(int id);
+        Task<Supplier> GetSupplierByProductAsync(int id);
+        Task<ICollection<Order>> GetOrdersByProductAsync(int id);
+    }
+}
