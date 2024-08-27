5f51644fff3ab0cc127e832a19c2783565fea9f0
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-28 00:38:27 +0300

    Add ITerritoryRepository

---
+++ Repositories/IRepository/ITerritoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface ITerritoryRepository : IEntityBaseRepository<Territory>
+    {
+        Task<ICollection<Employee>> GetEmployeesByTerritoryAsync(int id);
+        Task<Region> GetRegionByTerritoryAsync(int id);
+    }
+}
