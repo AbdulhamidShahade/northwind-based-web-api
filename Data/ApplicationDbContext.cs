927d411fc40e8e831e8d0d83b5b15f06c979a3a3
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-29 23:34:51 +0300

    Update delete behavior to cascase

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -34,86 +34,80 @@ namespace NorthwindBasedWebAPI.Data
                 .HasMany(p => p.Products)
                 .WithOne(c => c.Category)
                 .HasForeignKey(fk => fk.CategoryId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<Customer>()
                 .HasMany(o => o.Orders)
                 .WithOne(c => c.Customer)
                 .HasForeignKey(fk => fk.CustomerId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<CustomerCustomerDemographic>()
                 .HasOne(c => c.Customer)
                 .WithMany(m => m.CustomerCustomerDemographic)
                 .HasForeignKey(fk => fk.CustomerId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<CustomerCustomerDemographic>()
                 .HasOne(t => t.CustomerType)
                 .WithMany(m => m.CustomerCustomerDemographic)
                 .HasForeignKey(fk => fk.CustomerTypeId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<Employee>()
                 .HasMany(o => o.Orders)
                 .WithOne(e => e.Employee)
                 .HasForeignKey(fk => fk.EmployeeId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Employee>()
-                .HasOne(rt => rt.Report)
-                .WithMany()
-                .HasForeignKey(fk => fk.ReportsTo)
-                .OnDelete(DeleteBehavior.NoAction);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<EmployeeTerritory>()
                 .HasOne(e => e.Employee)
                 .WithMany(m => m.EmployeeTerritory)
                 .HasForeignKey(fk => fk.EmployeeId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<EmployeeTerritory>()
                 .HasOne(t => t.Territory)
                 .WithMany(m => m.EmployeeTerritory)
                 .HasForeignKey(fk => fk.TerritoryId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<Order>()
                 .HasOne(s => s.Shipper)
                 .WithMany(m => m.Orders)
                 .HasForeignKey(fk => fk.ShipVia)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<OrderDetails>()
                 .HasOne(p => p.Product)
                 .WithMany(m => m.OrderDetails)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<OrderDetails>()
                 .HasOne(o => o.Order)
                 .WithMany(m => m.OrderDetails)
                 .HasForeignKey(fk => fk.OrderId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<Product>()
                 .HasOne(s => s.Supplier)
                 .WithMany(p => p.Products)
                 .HasForeignKey(fk => fk.SupplierId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
 
 
             modelBuilder.Entity<Region>()
                 .HasMany(t => t.Territories)
                 .WithOne(r => r.Region)
                 .HasForeignKey(fk => fk.RegionId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
