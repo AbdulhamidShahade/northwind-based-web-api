ce9a197032adecab6e4428246b5be4405a23fb87
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-30 23:03:00 +0300

    Add IUserRepository.cs

---
+++ Repositories/IRepository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebApplication.API.Models;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<bool> CreateAsync(ApplicationUser user);
+        Task<bool> DeleteAsync(int id);
+        Task<IReadOnlyList<ApplicationUser>> GetAllAsync();
+        Task<ApplicationUser> GetByIdAsync(int id);
+        Task<bool> IsExistsAsync(int id);
+        Task<bool> IsExistsAsync(string email);
+        Task<bool> ChangePasswordAsync(ApplicationUser user, string currentPassword, string newPassword);
+        Task<bool> UpdateAsync(ApplicationUser user);
+    }
+}
