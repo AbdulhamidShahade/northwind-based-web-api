be9393a249ac3b069624bd447423bd3a37646d66
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-31 00:11:51 +0300

    Update supplier controller with fixing namespaces

--- Controllers/SuppliersController.cs
+++ Controllers/SuppliersController.cs
@@ -1,16 +1,16 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using NorthwindBasedWebApplication.API.Models.DTOs.SupplierDTOs;
 using NorthwindBasedWebApplication.API.Models;
 using System.Net;
-using NorthwindBasedWebApplication.API.Models.DTOs.ProductDTOs;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using NorthwindBasedWebAPI.Repositories.IRepository;
 using NorthwindBasedWebAPI.Models.Common;
 using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Models.Dtos.SupplierDtos;
+using NorthwindBasedWebAPI.Models.Dtos.ProductDtos;
 
-namespace NorthwindBasedWebApplication.API.Controllers
+namespace NorthwindBasedWebAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
