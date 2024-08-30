e5988313306d241fcf267271d260a69a25d366a9
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-30 23:00:52 +0300

    Add RegisterRequestDto.cs

---
+++ Models/Dtos/AuthDTOs/RegisterRequestDto.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+using NorthwindBasedWebAPI.Models.Dtos.UserDtos;
+
+namespace NorthwindBasedWebAPI.Models.Dtos.AuthDtos
+{
+    public class RegisterRequestDto
+    {
+        public UserDto user { get; set; }
+    }
+}
