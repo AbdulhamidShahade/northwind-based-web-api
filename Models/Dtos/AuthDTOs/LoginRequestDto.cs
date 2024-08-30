41cbb78222a972581c128293f3c6e53e72b3f53e
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-30 22:59:32 +0300

    Add LoginRequestDto.cs

---
+++ Models/Dtos/AuthDTOs/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NorthwindBasedWebAPI.Models.Dtos.AuthDtos
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
