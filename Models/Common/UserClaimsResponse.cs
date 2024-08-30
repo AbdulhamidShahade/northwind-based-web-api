86ddc0f7c220c2edb6f54d382be4a5c6be91c6d6
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-30 23:01:31 +0300

    Add UserClaimsResponse.cs

---
+++ Models/Common/UserClaimsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace NorthwindBasedWebAPI.Models.Common
+{
+    public class UserClaimsResponse
+    {
+        public int UserId { get; set; }
+        public List<UserClaims> Claims { get; set; }
+    }
+}
