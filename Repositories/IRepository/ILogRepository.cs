a82e0ccf93223087692e529ae19ccd116798279e
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-31 15:23:09 +0300

    Add ILogRepository

---
+++ Repositories/IRepository/ILogRepository.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.Base;
+
+namespace NorthwindBasedWebAPI.Repositories.IRepository
+{
+    public interface ILogRepository
+    {
+        public List<SystemLog> GetAll();
+    }
+}
