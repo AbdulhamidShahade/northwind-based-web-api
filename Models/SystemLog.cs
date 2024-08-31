6abc8da164330bb0f8f6c35a05633c5fe2207f36
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-08-31 15:23:44 +0300

    Add SystemLogs.cs model

---
+++ Models/SystemLog.cs
@@ -0,0 +1,23 @@
+﻿using NorthwindBasedWebAPI.Models.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindBasedWebAPI.Models
+{
+    public class SystemLog : BaseEntity
+    {
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string MethodType { get; set; }
+        public string User { get; set; }
+        public string Role { get; set; }
+        public string Details { get; set; }
+        public string StatusCode { get; set; }
+        public bool? Success { get; set; }
+        public bool? Failed { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        [NotMapped]
+        public override DateTime? UpdatedAt { get; set; }
+    }
+}
