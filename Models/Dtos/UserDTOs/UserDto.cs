bbb20fff9e98d5950bc5556fefa71f56c4d2e730
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-05 23:07:36 +0300

    Add confirm password property

--- Models/Dtos/UserDTOs/UserDto.cs
+++ Models/Dtos/UserDTOs/UserDto.cs
@@ -1,9 +1,14 @@
-﻿namespace NorthwindBasedWebAPI.Models.Dtos.UserDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebAPI.Models.Dtos.UserDtos
 {
     public class UserDto
     {
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        [Required, Compare(nameof(Password))]
+        public string ConfiremPassword { get; set; }
     }
 }
