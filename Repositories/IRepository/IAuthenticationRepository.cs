d530f766ef9a7b9bf52fe10c19fc4fdd84007f22
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-05 23:03:48 +0300

    Add functionalities to IAuthenticationRepository.cs

--- Repositories/IRepository/IAuthenticationRepository.cs
+++ Repositories/IRepository/IAuthenticationRepository.cs
@@ -1,6 +1,8 @@
-﻿using NorthwindBasedWebAPI.Models.Dtos.AuthDtos;
+﻿using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Models.Dtos.AuthDtos;
 using NorthwindBasedWebAPI.Models.Dtos.UserDtos;
 using NorthwindBasedWebApplication.API.Models;
+using System.Threading.Tasks;
 
 
 namespace NorthwindBasedWebAPI.Repositories.IRepository
@@ -10,5 +12,9 @@ namespace NorthwindBasedWebAPI.Repositories.IRepository
         Task<bool> IsUniqueUser(UserDto user);
         Task<RegisterResponseDto> Register(RegisterRequestDto registerRequestDto);
         Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<ApplicationUser> GetByUserNameAsync(string name);
+        Task<ApplicationUser> GetByEmailAsync(string email);
+        bool IsAuthenticatedUser(string? email = null, string? userName = null);
+
     }
 }
