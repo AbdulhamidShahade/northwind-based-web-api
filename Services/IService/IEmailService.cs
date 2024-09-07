197bfb0e684a07fa08ffe128d23b511678e24b9b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-07 23:45:17 +0300

    Add IEmailService.cs

---
+++ Services/IService/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using MimeKit;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Models.Common;
+
+namespace NorthwindBasedWebAPI.Services.IService
+{
+    public interface IEmailService
+    {
+        void SendEmail(MimeMessage message);
+        void SendEmailConfirmed(string emailConfirmed);
+        void ResetPassword(ApplicationUser user, string newPassword, string token);
+        void ConfirmEmail(string email);
+
+    }
+}
