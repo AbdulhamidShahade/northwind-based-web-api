674663aeb361c39687fd2b920b32df55c1e84126
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-07 23:45:05 +0300

    Add message model

---
+++ Models/Common/Message.cs
@@ -0,0 +1,18 @@
+﻿using MimeKit;
+
+namespace NorthwindBasedWebAPI.Models.Common
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            this.Subject = subject;
+            this.Content = content;
+            this.To = new List<MailboxAddress>();
+            this.To.AddRange(to.Select(x => new MailboxAddress("email", x)));
+        }
+    }
+}
