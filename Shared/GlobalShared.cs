afb25785d6aef6b9d63e40f7e3a19fd5a2c31a9c
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-11 22:49:10 +0300

    Add GlobalShared.cs

---
+++ Shared/GlobalShared.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NorthwindBasedWebAPI.Models;
+
+namespace NorthwindBasedWebAPI.Shared
+{
+    public class GlobalShared
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GlobalShared(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<string> GetUserRole(string? email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return "NULL";
+            }
+
+            return _userManager.GetRolesAsync(user).GetAwaiter().GetResult().FirstOrDefault();
+        }
+    }
+}
