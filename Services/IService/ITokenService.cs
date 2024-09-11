7f72a6e027bd0a77574c4f3e511e9826a6fb2c60
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-11 22:48:51 +0300

    Add ITokenService.cs

---
+++ Services/IService/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBasedWebAPI.Models;
+
+namespace NorthwindBasedWebAPI.Services.IService
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateEmailConfirmationTokenAsync(ApplicationUser user);
+        Task<string> GeneratePasswordResetTokenAsync(ApplicationUser user);
+
+    }
+}
