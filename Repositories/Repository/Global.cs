aba4b42765ab20756757d342280b7d1a6367ce9f
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-11 22:44:05 +0300

    Add Global concrete class

---
+++ Repositories/Repository/Global.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Repositories.IRepository;
+
+namespace NorthwindBasedWebAPI.Repositories.Repository
+{
+    public class Global : IGlobal
+    {
+        public string Email { get; set; } = "Anonymous";
+        public string Role { get; set; } = "Undefined";
+    }
+}
