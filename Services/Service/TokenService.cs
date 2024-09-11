5c661507c83b11a684dc1deadccda56959519cdc
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-09-11 22:49:00 +0300

    Add TokenService.cs

---
+++ Services/Service/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NorthwindBasedWebAPI.Models;
+using NorthwindBasedWebAPI.Services.IService;
+
+namespace NorthwindBasedWebAPI.Services.Service
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TokenService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateEmailConfirmationTokenAsync(ApplicationUser user)
+        {
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            return token;
+        }
+
+        public async Task<string> GeneratePasswordResetTokenAsync(ApplicationUser user)
+        {
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            return token;
+        }
+
+
+    }
+}
